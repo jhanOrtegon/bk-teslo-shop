d297b1c02490d8520597ccc8a905e4da736d3fed
Jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2025-03-08 00:30:13 -0500

    first commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,33 @@
+import {
+    IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength
+} from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string
+
+    @IsString()
+    @MinLength(10)
+    description: string;
+
+    @IsString()
+    slug: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[];
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductsService {
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) { }
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const procduct = this.productRepository.create(createProductDto);
+      await this.productRepository.save(procduct);
+      return procduct;
+
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('algo salio mal');
+    }
+  }
+
+  findAll() {
+    return this.productRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
